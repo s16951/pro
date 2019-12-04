dc814aec11f22c5a4c0c45efbba3d184cb7ed958
Kamil Bielecki <s16951@pjwstk.edu.pl>
2019-12-04 17:10:42 +0100

    Add project files.

---
+++ EmpTest/Controllers/EmpsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmpTest.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpsController : ControllerBase
+    {
+        private s16951Context _context;
+        public EmpsController(s16951Context context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult GetEmps()
+        {
+            return Ok(_context.Emp.ToList());
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetEmp(int id)
+        {
+            var emp = _context.Emp.FirstOrDefault(e => e.Empno == id);
+
+            if (emp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(emp);
+        }
+
+    }
+}
---
+++ EmpTest/Models/s16951Context.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EmpTest.Models
+{
+    public partial class s16951Context : DbContext
+    {
+        public s16951Context()
+        {
+        }
+
+        public s16951Context(DbContextOptions<s16951Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Emp> Emp { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s16951;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Emp>(entity =>
+            {
+                entity.HasKey(e => e.Empno)
+                    .HasName("PK__EMP__14CCF2EEB6EE4B25");
+
+                entity.ToTable("EMP");
+
+                entity.Property(e => e.Empno)
+                    .HasColumnName("EMPNO")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Comm).HasColumnName("COMM");
+
+                entity.Property(e => e.Deptno).HasColumnName("DEPTNO");
+
+                entity.Property(e => e.Ename)
+                    .HasColumnName("ENAME")
+                    .HasMaxLength(10)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Hiredate)
+                    .HasColumnName("HIREDATE")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.Job)
+                    .HasColumnName("JOB")
+                    .HasMaxLength(9)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mgr).HasColumnName("MGR");
+
+                entity.Property(e => e.Sal).HasColumnName("SAL");
+            });
+        }
+    }
+}
